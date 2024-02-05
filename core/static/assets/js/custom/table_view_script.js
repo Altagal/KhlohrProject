663f090e726ded8e9ccaa19a48a3d10a76058e1a
Gustavo <gustavojordao@outlook.com>
2024-02-05 11:56:50 -0300

    Table scripts and view abstracted and fixed.

--- core/static/assets/js/custom/table_view_script.js
+++ core/static/assets/js/custom/table_view_script.js
@@ -1,60 +1,41 @@
 //DATATABLE
 $(document).ready(function() {
-			$('.all-spells-table').DataTable({
-			    paging: true,
-			    pageLength: 10,
-			    bLengthChange: false,
-			    columnDefs: [{ orderable: false, targets: -1 }, { orderable: false, targets: 5 }],
-			    info: false,
-            } );
-		});
+    $('.all-spells-table').DataTable({
+	    paging: true,
+		pageLength: 10,
+		bLengthChange: false,
+		columnDefs: [{ orderable: false, targets: -1 }, { orderable: false, targets: 5 }],
+		info: false,
+    });
+});
 
 $(document).ready(function() {
-			$('.spells-table').DataTable({
-			    paging: true,
-			    pageLength: 10,
-			    bLengthChange: false,
-			    columnDefs: [{ orderable: false, targets: -1 }, { orderable: false, targets: 4 }],
-			    info: false,
-            } );
-		});
+    $('.spells-table').DataTable({
+	    paging: true,
+		pageLength: 10,
+		bLengthChange: false,
+		columnDefs: [{ orderable: false, targets: -1 }, { orderable: false, targets: 4 }],
+		info: false,
+    });
+});
 
 $(document).ready(function() {
-			$('.spell-school-table').DataTable({
-			    paging: true,
-			    pageLength: 10,
-			    bLengthChange: false,
-			    columnDefs: [{ orderable: false, targets: -1 }, ],
-			    info: false,
-			    searching:true,
-
-            } );
-		});
-$(document).ready(function() {
-			$('.all-item-table').DataTable({
-			    paging: true,
-			    pageLength: 10,
-			    bLengthChange: false,
-			    columnDefs: [{ orderable: false, targets: -1 },],
-			    info: false,
-            } );
-		});
+    $('.spell-school-table').DataTable({
+	    paging: true,
+		pageLength: 10,
+		bLengthChange: false,
+		columnDefs: [{ orderable: false, targets: -1 }, ],
+		info: false,
+		searching:true,
+    });
+});
 
 $(document).ready(function() {
-			$('.item-table').DataTable({
-			    paging: true,
-			    pageLength: 10,
-			    bLengthChange: false,
-			    columnDefs: [{ orderable: false, targets: -1 }],
-			    info: false,
-            } );
-		});
-$(document).ready(function() {
-			$('.infusion-table').DataTable({
-			    paging: true,
-			    pageLength: 10,
-			    bLengthChange: false,
-			    columnDefs: [{ orderable: false, targets: -1 }],
-			    info: false,
-            } );
-		});
+    $('.generic-table').DataTable({
+	    paging: true,
+		pageLength: 10,
+		bLengthChange: false,
+		columnDefs: [{ orderable: false, targets: -1 }],
+		info: false,
+    });
+});
