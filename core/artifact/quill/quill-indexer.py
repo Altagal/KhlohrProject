3631d90a9c4310e77eb1b64a8b2511f47919a496
gustavo.jordao <gustavojordao@outlook.com>
2024-04-03 09:20:34 -0300

    Melhoria no indexer

--- core/artifact/quill/quill-indexer.py
+++ core/artifact/quill/quill-indexer.py
@@ -12,14 +12,14 @@ except Exception as e:
 try:
     with open("core/artifact/quill/index-results.json", 'w') as index_output_f:
         for card in card_data:
-            index_output_f.write("  {\n")
+            index_output_f.write("{\n")
             index_output_f.write('    "slug": "' + card["slug"] + '",\n')
-            index_output_f.write('    "type": "' + type_name + '"\n')
-            index_output_f.write("  },\n")
+            index_output_f.write('    "name": "' + card["name"] + '",\n')
+            index_output_f.write('    "type": {\n      "slug": "' + card["type"]["slug"] + '",\n      "name": "' + card["type"]["name"] + '"\n  }\n')
+            index_output_f.write("},\n")
 
 except Exception as e:
     print("open - test_index.json")
     print(e)
-
 
 print("Finalizado")
