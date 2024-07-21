847a6c1a814fe40e0535bd1e0e81c2f925d7ae04
Gustavo Macêdo Jordão <gustavojordao@outlook.com>
2024-07-21 13:03:50 -0300

    fix - n precisa de all-spells-table

--- core/static/assets/js/custom/table_view_script.js
+++ core/static/assets/js/custom/table_view_script.js
@@ -1,20 +1,10 @@
 //DATATABLE
 $(document).ready(function() {
-    $('.all-spells-table').DataTable({
-	    paging: true,
-		pageLength: 10,
-		bLengthChange: false,
-		columnDefs: [{ orderable: false, targets: -1 }, { orderable: false, targets: 5 }],
-		info: false,
-    });
-});
-
-$(document).ready(function() {
     $('.spells-table').DataTable({
 	    paging: true,
 		pageLength: 10,
 		bLengthChange: false,
-		columnDefs: [{ orderable: false, targets: -1 }, { orderable: false, targets: 4 }],
+		columnDefs: [{ orderable: false, targets: -1 }, { orderable: false, targets: -3 }],
 		info: false,
     });
 });
