213382644365683f1bdcc98595d1b34f82840a2b
Gustavo Macêdo Jordão <gustavojordao@outlook.com>
2024-07-21 13:59:25 -0300

    fix - padronização das tables e simplificação da chamada do card magic_school

--- cardviewer/views.py
+++ cardviewer/views.py
@@ -20,15 +20,10 @@ def card_view(request, card_slug):
 
     # IF MAGIC SCHOOL
     if card["type"]["slug"] == "magic_school":
-        magic_school_spell_list = []
-        for spell_card in get_deck("spell"):
-            # Se spell.magic_scholl é igual a magic_school slug
-            if spell_card["magic_school"]["slug"] == card["slug"]:
-                magic_school_spell_list.append(spell_card)
         context = {
             'card': card,
-            'range': range(10),
-            'table_list': magic_school_spell_list,
+            "spell_level_range": range(10),
+            'table_list': get_deck("spell"),
         }
     # IF CLASS
     elif card["type"]["slug"] == "class":
@@ -71,7 +66,7 @@ def condition_list(request):
 
 def spell_list(request):
     context = {
-        "spell_level_range": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        "spell_level_range": range(10),
         "table_list": get_deck("spell"),
     }
 
